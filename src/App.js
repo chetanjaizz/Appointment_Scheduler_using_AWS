600af926cbbb46de6319901aecf6cd27e90fe5a1
chetanjaizz <chetanjaiswal2306@gmail.com>
2024-06-23 13:11:43 +0530

    assignment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Dashboard from './components/Dashboard';
+import Mainheader from './components/Mainheader';
+// import {backg} from "./public/backg.png";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  return <div>
+    <Dashboard />
+  </div>
+
 }
 
 export default App;
